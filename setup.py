88bf6c520cf364496e15302f79bb6b6b868cf0ac
David <davidyen1124@gmail.com>
2014-10-28 01:32:49 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,14 @@ from distutils.core import setup
 
 setup(
     name='gooimage',
+    version='0.0.3',
+    author='David',
+    author_email='davidyen1124@gmail.com',
+    description='Search google images in Python',
+    keywords='google image search',
+    url='https://github.com/davidyen1124/Google-Image',
     packages=['gooimage'],
+    install_requires=[
+        'requests==2.4.3',
+    ],
 )
